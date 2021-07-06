b1dd77f2ba2bde29171833394e7ebbd3aec5b768
mina <mina.adibe@gmal.com>
2021-07-06 07:52:28 +0200

    added dynamic routes of product card

--- src/component/App.js
+++ src/component/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import PageNotFound from "./PageNotFound";
+import { ProductsList, ProductPage } from "./index";
+
 function App() {
   return (
-    <div className="bg-gray-400">
-      <h2>hello world </h2>
+    <div>
+      <Switch>
+        <Route exact path="/" component={ProductsList} />
+        <Route path="/products/:id" component={ProductPage} />
+
+        <Route component={PageNotFound} />
+        <ProductsList />
+      </Switch>
     </div>
   );
 }
--- src/component/index.js
+++ src/component/index.js
@@ -1,2 +1,4 @@
 export { default as ProductCard } from "./productCard/Index";
 export { default as ProductsList } from "./productsList/Index";
+export { default as ProductPage } from "./productPage/Index";
+export { default as PageNotFound } from "./PageNotFound";
