31649f24d6994b9178b2f50bf5c7d48410f891eb
mina <mina.adibe@gmal.com>
2021-07-06 08:08:42 +0200

    added links to card image and title

--- src/component/productCard/Index.js
+++ src/component/productCard/Index.js
@@ -1,29 +1,34 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const productCard = ({ category, title, description, price, image }) => {
+const productCard = ({ category, title, price, image, id }) => {
   return (
     <div>
       <div className="antialiased  text-gray-900 font-sans p-6">
         <div className="container mx-auto">
           <div className="flex flex-wrap -mx-4">
             <div className="w-full    p-4">
-              <a
+              <div
                 href="/"
                 className="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden"
               >
                 <div className="relative pb-48 overflow-hidden">
-                  <img
-                    className="absolute inset-0 h-full w-full object-cover"
-                    src={image}
-                    alt=""
-                  />
+                  <Link to={`/products/${id}`}>
+                    <img
+                      className="absolute inset-0 h-full w-full object-cover"
+                      src={image}
+                      alt=""
+                    />
+                  </Link>
                 </div>
                 <div className="p-4">
                   <span className="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs">
                     {category}
                   </span>
-                  <h2 className="mt-2 mb-2  font-bold">{title}</h2>
-                  <p className="text-sm">{description}</p>
+                  <h2 className="mt-2 mb-2  font-bold">
+                    <Link to={`/products/${id}`}>{title}</Link>
+                  </h2>
+
                   <div className="mt-3 flex items-center"></div>
                 </div>
                 <div className="p-4 border-t border-b text-xs text-gray-700">
@@ -32,7 +37,7 @@ const productCard = ({ category, title, description, price, image }) => {
                     add to cart
                   </button>
                 </div>
-              </a>
+              </div>
             </div>
           </div>
         </div>
