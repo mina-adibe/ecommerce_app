e6002e991f34abf5a996d82d542070f8aafe01f3
mina <mina.adibe@gmal.com>
2021-07-09 21:57:54 +0200

    add routes

--- src/component/App.js
+++ src/component/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import PageNotFound from "./PageNotFound";
-import { ProductsList, ProductPage } from "./index";
+import {
+  ProductsList,
+  ProductPage,
+  PageNotFound,
+  Header,
+  Checkout,
+} from "./index";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={ProductsList} />
+        <Route exact path="/checkout" component={Checkout} />
         <Route path="/products/:id" component={ProductPage} />
-
         <Route component={PageNotFound} />
-        <ProductsList />
       </Switch>
     </div>
   );
