21e484d21298ae564f100735b1e979314923c6f4
mina <mina.adibe@gmal.com>
2021-07-09 21:57:30 +0200

    replace static api with firebase

--- src/api/productApi.js
+++ src/api/productApi.js
@@ -2,7 +2,10 @@ import { handleResponse, handleError } from "./apiUtils";
 const baseUrl = process.env.API_URL;
 
 export function getProducts() {
-  return fetch("https://fakestoreapi.com/products")
+  //return fetch("https://fakestoreapi.com/products")
+  return fetch(
+    "https://us-central1-shopping-cart-ff20c.cloudfunctions.net/user"
+  )
     .then(handleResponse)
     .catch(handleError);
 }
