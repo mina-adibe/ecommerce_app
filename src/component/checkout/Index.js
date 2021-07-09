0700949c4446e26a7346c4944c575769ca0582b8
mina <mina.adibe@gmal.com>
2021-07-09 23:34:27 +0200

    added cart summary

--- src/component/checkout/Index.js
+++ src/component/checkout/Index.js
@@ -8,7 +8,7 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import { clearCart } from "../../redux/actions/cartActions";
 
-const Checkout = ({ clearCart }) => {
+const Checkout = ({ clearCart, cartItems }) => {
   const formik = useFormik({
     initialValues: {
       address: "",
@@ -30,83 +30,108 @@ const Checkout = ({ clearCart }) => {
   });
 
   return (
-    <div className="w-1/2 mx-auto ">
+    <div className="w-3/4 mx-auto ">
       <h1 className="text-center text-5xl text-gray-600 my-11">Review Order</h1>
+      <div className="grid grid-cols-2">
+        <div className="mx-10">
+          <form
+            className="space-y-4 text-gray-700"
+            onSubmit={formik.handleSubmit}
+          >
+            <div className="flex flex-wrap">
+              <div className="w-full">
+                <label className="block mb-1" for="formGridCode_card">
+                  Address
+                </label>
+                <input
+                  className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                  type="text"
+                  name="address"
+                  value={formik.values.address}
+                  onChange={formik.handleChange}
+                  id="formGridCode_card"
+                />
+                {formik.errors.address && formik.touched.address && (
+                  <p className="text-red-600">{formik.errors.address}</p>
+                )}
+              </div>
+            </div>
 
-      <form className="space-y-4 text-gray-700" onSubmit={formik.handleSubmit}>
-        <div className="flex flex-wrap">
-          <div className="w-full">
-            <label className="block mb-1" for="formGridCode_card">
-              Address
-            </label>
-            <input
-              className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-              type="text"
-              name="address"
-              value={formik.values.address}
-              onChange={formik.handleChange}
-              id="formGridCode_card"
-            />
-            {formik.errors.address && formik.touched.address && (
-              <p className="text-red-600">{formik.errors.address}</p>
-            )}
+            <div className="flex flex-wrap">
+              <div className="w-full">
+                <label className="block mb-1" for="formGridCode_card">
+                  Phone
+                </label>
+
+                <PhoneInput
+                  inputStyle={{
+                    width: "100%",
+                    height: "2.5rem ",
+                    borderRadius: "0.5rem",
+                    fontSize: "1rem",
+                    lineHeight: "1.5rem",
+                    borderWidth: "1px",
+                  }}
+                  country={"eg"}
+                  type="number"
+                  name="phone"
+                  value={formik.values.phone}
+                  onChange={(e) => formik.setFieldValue("phone", e)}
+                />
+
+                {formik.errors.phone && formik.touched.phone && (
+                  <p className="text-red-600">{formik.errors.phone}</p>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-wrap">
+              <div className="w-full">
+                <label className="block mb-1" for="formGridCode_card">
+                  Email
+                </label>
+                <input
+                  className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
+                  type="email"
+                  name="email"
+                  value={formik.values.email}
+                  onChange={formik.handleChange}
+                  id="formGridCode_card"
+                />
+                {formik.errors.email && formik.touched.email && (
+                  <p className="text-red-600">{formik.errors.email}</p>
+                )}
+              </div>
+              <div className="my-7 ">
+                <button
+                  type="submit"
+                  className="bg-gray-400 text-lg px-4 py-2  border-gray-600 rounded-lg hover:bg-gray-300"
+                >
+                  Submit
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div className="mx-10 text-gray-700 text-xl">
+          <h2>cart Summary</h2>
+          <div>
+            {cartItems.map((item, index) => (
+              <div key="item.key">
+                <p>item: {index}</p>
+                <p>title:{item.title}</p>
+                <p>
+                  price: {item.price} X No: {item.quantity} =
+                  <span className="text-red-700 text-2xl">
+                    {" "}
+                    {item.price * item.quantity}
+                  </span>
+                </p>
+                <p>--------</p>
+              </div>
+            ))}
           </div>
         </div>
-
-        <div className="flex flex-wrap">
-          <div className="w-full">
-            <label className="block mb-1" for="formGridCode_card">
-              Phone
-            </label>
-
-            <PhoneInput
-              inputStyle={{
-                width: "100%",
-                height: "2.5rem ",
-                borderRadius: "0.5rem",
-                fontSize: "1rem",
-                lineHeight: "1.5rem",
-                borderWidth: "1px",
-              }}
-              country={"eg"}
-              type="number"
-              name="phone"
-              value={formik.values.phone}
-              onChange={(e) => formik.setFieldValue("phone", e)}
-            />
-
-            {formik.errors.phone && formik.touched.phone && (
-              <p className="text-red-600">{formik.errors.phone}</p>
-            )}
-          </div>
-        </div>
-        <div className="flex flex-wrap">
-          <div className="w-full">
-            <label className="block mb-1" for="formGridCode_card">
-              Email
-            </label>
-            <input
-              className="w-full h-10 px-3 text-base placeholder-gray-600 border rounded-lg focus:shadow-outline"
-              type="email"
-              name="email"
-              value={formik.values.email}
-              onChange={formik.handleChange}
-              id="formGridCode_card"
-            />
-            {formik.errors.email && formik.touched.email && (
-              <p className="text-red-600">{formik.errors.email}</p>
-            )}
-          </div>
-          <div className="my-7 ">
-            <button
-              type="submit"
-              className="bg-gray-400 text-lg px-4 py-2  border-gray-600 rounded-lg hover:bg-gray-300"
-            >
-              Submit
-            </button>
-          </div>
-        </div>
-      </form>
+      </div>
     </div>
   );
 };
@@ -114,8 +139,17 @@ const mapDispatchToProps = {
   clearCart,
 };
 
+function mapStateToProps(state, ownProps) {
+  return {
+    cartItems: state.cart,
+  };
+}
+
 Checkout.propTypes = {
   clearCart: PropTypes.func.isRequired,
 };
+Checkout.defaultProps = {
+  cartItems: [],
+};
 
-export default connect(null, mapDispatchToProps)(Checkout);
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout);
